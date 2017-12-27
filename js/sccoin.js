5142bd9dcaaa683c34f1710e740cb2ac9c6a9547
scamconnect <34539859+scamconnect@users.noreply.github.com>
2017-12-27 14:47:59 +0200

    Website

---
+++ js/sccoin.js
@@ -0,0 +1,87 @@
+console.log("nothing to see here, move along");
+
+function decodeStats(response, price) {
+    if (response == null) return null;
+
+    var result = response.result;
+    if (result == null || result.length == null || result.length < 193) return null;
+
+    var weiPerEther = new BigNumber("1000000000000000000", 10);
+
+    var totalContributionExact = new BigNumber(result.substr(2, 64), 16).div(weiPerEther);
+    var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
+
+    return {
+        totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
+        totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
+        totalContributionFOODs: totalContributionUSDExact.div(new BigNumber("6")).round(0, BigNumber.ROUND_DOWN),
+        totalSupply: new BigNumber(result.substr(66, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        totalBonusTokensIssued: new BigNumber(result.substr(130, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        purchasingAllowed: new BigNumber(result.substr(194, 64), 16).isZero() == false
+    };
+}
+
+function getStats(price) {
+    var url = "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x320E735F4F13BE0c9a99cE871463eB1f51354857&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) { return decodeStats(data, price); });
+}
+
+function getPrice() {
+    var url = "https://api.etherscan.io/api?module=stats&action=ethprice";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) {
+        if (data == null) return null;
+        if (data.result == null) return null;
+        if (data.result.ethusd == null) return null;
+
+        return parseFloat(data.result.ethusd);
+    });
+}
+
+function updatePage(stats) {
+    if (stats == null) return;
+
+
+    $("#total-ether").text(stats.totalContribution.toFixed(3));
+    if (stats.totalContribution.toNumber() <= 0) {
+        $("#total-ether-message").text("Looks like none cares about helping kitties.");
+    } else {
+        $("#total-ether-message").text("So much love for the kitties!");
+    }
+
+    $("#total-usd").text("$" + stats.totalContributionUSD.toFixed(0));
+    if (stats.totalContributionUSD.toNumber() <= 0) {
+        $("#total-usd-message").text("No Ether yet, so no food for the kitties :(");
+    } else if (stats.totalContributionFOODs.toNumber() < 1) {
+        $("#total-usd-message").text("Not enough to a bag of food for the kitties :(");
+    }else if (stats.totalContributionFOODs.toNumber() < 2) {
+        $("#total-usd-message").text("Just enough to buy a bag of food for the kitties");
+    } else {
+        $("#total-usd-message").text("Enough to buy " + stats.totalContributionFOODs.toFixed(0) + " bags of food for the kitties!");
+    }
+
+    $("#total-tokens").text(stats.totalSupply.toFixed(3));
+    if (stats.totalSupply <= 0) {
+        $("#total-tokens-message").text("No Scam Connect coins issued.");
+    } else if (stats.totalBonusTokensIssued.toNumber() <= 0) {
+        $("#total-tokens-message").text("So many Scam Connect coins!");
+    } else {
+        $("#total-tokens-message").text("Including " + stats.totalBonusTokensIssued.toFixed(3) + " bonus tokens!");
+    }
+
+    $("#stats").show();
+}
+
+function refresh() { getPrice().then(getStats).then(updatePage); }
+
+$(function() {
+    try {
+        refresh();
+        setInterval(refresh, 1000 * 60 * 5);
+    } catch (err) { }
+});
